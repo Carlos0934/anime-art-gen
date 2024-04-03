8c57f62c973543a950fb7a87cd11c397b0c8416a
Carlos Olivo <carlosengel011@gmail.com>
2024-04-03 17:47:42 -0400

    Update basePath in serverless.yml

---
+++ src/modules/auth/tests/login-user-handler.spec.ts
@@ -0,0 +1,7 @@
+import { describe, test } from "node:test";
+
+describe("LoginUserHandler", () => {
+  test("should return a token when the user is logged in", () => {
+    // test implementation
+  });
+});
