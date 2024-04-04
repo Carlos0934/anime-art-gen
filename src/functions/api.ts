7868a212b59d58071139b66ee043555b7649cee2
Carlos Olivo <carlosengel011@gmail.com>
2024-04-03 22:33:11 -0400

    Add PaymentModule to createHonoApp

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -1,5 +1,6 @@
 import { AuthModule } from "@/auth/module";
 import { createHonoApp } from "@/core/http/server";
+import { PaymentModule } from "@/payment/module";
 import { handle } from "hono/aws-lambda";
 
 const app = createHonoApp([AuthModule, PaymentModule]);
