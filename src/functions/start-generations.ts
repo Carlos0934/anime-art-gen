c7f3e8326b6ff8910e689b53969b9a830a36684f
Carlos Olivo <carlosengel011@gmail.com>
2024-04-08 12:17:26 -0400

    Add AWS SDK clients for SNS, SQS, SES, and DynamoDB

---
+++ src/functions/start-generations.ts
@@ -0,0 +1,8 @@
+import { SQSEvent } from "aws-lambda";
+
+exports.handler = async (event: SQSEvent): Promise<void> => {
+  for (const record of event.Records) {
+    const { body } = record;
+    console.log(body);
+  }
+};
