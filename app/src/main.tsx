8bba739e1d24a9e23e403bee25ceb7d777d5a4c2
Carlos Engel Olivo <carlosengel011@gmail.com>
2024-04-24 16:25:49 -0400

    Change npm to pnpm to install dependencies

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,12 +5,17 @@ import "./index.css";
 import { Route, Switch } from "wouter";
 import { HomePage } from "./pages/home-page.tsx";
 import { SignInPage } from "./pages/sign-in.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Switch>
-      <Route path="/" component={HomePage} />
-      <Route path="/sign-in" component={SignInPage} />
-    </Switch>
+    <QueryClientProvider client={queryClient}>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/sign-in" component={SignInPage} />
+      </Switch>
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ app/src/services/auth/sign-in.ts
@@ -0,0 +1,22 @@
+type SignInInput = {
+  email: string;
+  password: string;
+};
+export const signIn = async (data: SignInInput) => {
+  const url = `${import.meta.env.API_URL}/auth/login`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.status === 401) throw new Error("Invalid credentials");
+
+  if (!response.ok) throw new Error("An error occurred");
+
+  const json = (await response.json()) as { token: string };
+
+  return json.token;
+};
